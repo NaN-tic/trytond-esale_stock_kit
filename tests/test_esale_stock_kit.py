54498c3ec32ead8581b01a1d3b5b5c83f174aa40
resteve <resteve@zikzakmedia.com>
2015-12-01 11:07:46 +0100

    Tests

--- tests/test_esale_stock_kit.py
+++ tests/test_esale_stock_kit.py
@@ -1,32 +1,18 @@
-#!/usr/bin/env python
-# This file is part esale_stock_kit module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the esale_stock_kit module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class eSaleStockKitTestCase(unittest.TestCase):
-    'Test eSale Stock Kit module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('esale_stock_kit')
-
-    def test0005views(self):
-        'Test views'
-        test_view('esale_stock_kit')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class EsaleStockKitTestCase(ModuleTestCase):
+    'Test Esale Stock Kit module'
+    module = 'esale_stock_kit'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        eSaleStockKitTestCase))
+        EsaleStockKitTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
